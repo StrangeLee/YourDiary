a9e1692468e9798129946009d4e6454ef71c6543
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2020-07-26 16:20:04 +0900

    [Add] gps 권한 요청 코드

--- app/src/main/java/com/strange/yourdiary/ui/main/MainActivity.kt
+++ app/src/main/java/com/strange/yourdiary/ui/main/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.strange.yourdiary.ui.main
 
+import android.Manifest
 import android.content.Intent
-import android.os.Build
-import android.support.v7.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import com.strange.yourdiary.R
-import com.strange.yourdiary.db.AppDatabase
 import com.strange.yourdiary.ui.add.AddActivity
 import com.strange.yourdiary.ui.main.fragment.CalendarFragment
 import com.strange.yourdiary.ui.main.fragment.DiaryListFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val GPS_ENABLE_REQUEST_CODE = 2001
+    private val PERMISSIONS_REQUEST_CODE = 100
+
+    var REQUIRED_PERMISSIONS = arrayOf<String>(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,15 +64,35 @@ class MainActivity : AppCompatActivity() {
         bnv_diary.setOnNavigationItemSelectedListener {
             when(it.itemId) {
                 R.id.nav_add -> {
-                    var intent = Intent(this, AddActivity::class.java)
+                    val intent = Intent(this, AddActivity::class.java)
                     startActivity(intent)
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
-                        overridePendingTransition(0, 0)
-                    } // intent 시 화면전환 효과 없애줌
+                    overridePendingTransition(0, 0)
+                    // intent 시 화면전환 효과 없애줌
                     finish()
                 }
             }
             false
+        }
+
+        var permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        // 권한 체크
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            toast("권한 승인이 필요합니다.")
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSIONS_REQUEST_CODE && grantResults.size == REQUIRED_PERMISSIONS.size) {
+            toast("권한이 승인 되었습니다.")
+        } else {
+            toast("권한이 거부 되었습니다.")
         }
     }
 }
