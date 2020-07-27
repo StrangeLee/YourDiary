cec3bed18d4d577ad22455787122c19317f5e6e5
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2020-07-27 17:45:54 +0900

    [Fix] Gps 권한 요청 수정

--- app/src/main/java/com/strange/yourdiary/ui/main/MainActivity.kt
+++ app/src/main/java/com/strange/yourdiary/ui/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
@@ -15,14 +16,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
-
-    private val GPS_ENABLE_REQUEST_CODE = 2001
-    private val PERMISSIONS_REQUEST_CODE = 100
-
-    var REQUIRED_PERMISSIONS = arrayOf<String>(
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,12 +67,9 @@ class MainActivity : AppCompatActivity() {
             false
         }
 
-        var permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
 
-        // 권한 체크
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            toast("권한 승인이 필요합니다.")
-        }
+        // 위치 권한 체크
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION), 1)
     }
 
     override fun onRequestPermissionsResult(
@@ -88,11 +78,16 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == PERMISSIONS_REQUEST_CODE && grantResults.size == REQUIRED_PERMISSIONS.size) {
-            toast("권한이 승인 되었습니다.")
-        } else {
-            toast("권한이 거부 되었습니다.")
+        when (requestCode) {
+            1 -> {
+                for (i in permissions.indices) {
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        toast(permissions[i] + " 권한 승인됨.")
+                    } else {
+                        toast(permissions[i] + " 권한 승인 거부됨.")
+                    }
+                }
+            }
         }
     }
 }
