e70df677cb7b7d31facf8e9b1fbd75d7e3ce162d
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2020-07-29 12:26:37 +0900

    [Fix] add get gps location method

--- app/src/main/java/com/strange/yourdiary/ui/main/MainActivity.kt
+++ app/src/main/java/com/strange/yourdiary/ui/main/MainActivity.kt
@@ -3,19 +3,33 @@ package com.strange.yourdiary.ui.main
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.location.Location
 import android.os.Bundle
+import android.support.annotation.NonNull
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.strange.yourdiary.R
 import com.strange.yourdiary.ui.add.AddActivity
 import com.strange.yourdiary.ui.main.fragment.CalendarFragment
 import com.strange.yourdiary.ui.main.fragment.DiaryListFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
+
+    var REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +50,7 @@ class MainActivity : AppCompatActivity() {
             )
             .commit()
 
+        // toolbar 버튼 clickListener
         btn_calendar.setOnClickListener {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main_frame,
@@ -43,7 +58,6 @@ class MainActivity : AppCompatActivity() {
                 )
                 .commit()
         }
-
         btn_diary.setOnClickListener {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main_frame,
@@ -51,7 +65,6 @@ class MainActivity : AppCompatActivity() {
                 )
                 .commit()
         }
-
 
         // BottomNavigation
         bnv_diary.setOnNavigationItemSelectedListener {
@@ -67,9 +80,84 @@ class MainActivity : AppCompatActivity() {
             false
         }
 
+        checkRunTimePermission()
 
-        // 위치 권한 체크
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+        // get gps location
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                Log.d("TEST", "${location!!.latitude}, ${location.longitude}")
+            }
+            .addOnFailureListener {exception: Exception ->
+                exception.printStackTrace()
+                Log.e("TEST", exception.message)
+            }
+    }
+
+    fun checkRunTimePermission() {
+
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+            this@MainActivity,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            this@MainActivity,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        if (hasFineLocationPermission == PERMISSION_GRANTED &&
+            hasCoarseLocationPermission == PERMISSION_GRANTED
+        ) {
+
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+
+
+            // 3.  위치 값을 가져올 수 있음
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this@MainActivity,
+                    REQUIRED_PERMISSIONS.get(0)
+                )
+            ) {
+
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(this@MainActivity, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG)
+                    .show()
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(
+                    this@MainActivity, REQUIRED_PERMISSIONS,
+                    1
+                )
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(
+                    this@MainActivity, REQUIRED_PERMISSIONS,
+                    1
+                )
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -78,14 +166,37 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            1 -> {
-                for (i in permissions.indices) {
-                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                        toast(permissions[i] + " 권한 승인됨.")
-                    } else {
-                        toast(permissions[i] + " 권한 승인 거부됨.")
-                    }
+        if (requestCode == 1 && permissions.size == REQUIRED_PERMISSIONS.size) {
+
+            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+            var check_result = true
+
+
+            if (check_result) {
+                //위치 값을 가져올 수 있음
+            } else {
+                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        REQUIRED_PERMISSIONS.get(0)
+                    )
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        REQUIRED_PERMISSIONS.get(1)
+                    )
+                ) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    finish()
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
         }
