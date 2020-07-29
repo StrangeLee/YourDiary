f4d38ca3d958d6a9709bfe9b0a48b532ad70ada1
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2020-07-29 14:05:12 +0900

    [Fix] code clean

--- app/src/main/java/com/strange/yourdiary/view/activity/AddActivity.kt
+++ app/src/main/java/com/strange/yourdiary/view/activity/AddActivity.kt
@@ -1,12 +1,21 @@
 package com.strange.yourdiary.view.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.view.ContextThemeWrapper
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.strange.yourdiary.R
 import com.strange.yourdiary.data.DiaryData
 import com.strange.yourdiary.db.AppDatabase
@@ -25,6 +34,10 @@ class AddActivity : AppCompatActivity() {
     var curDay = calendar.get(Calendar.DAY_OF_WEEK).toString()
     var curTime = SimpleDateFormat("HH:mm").format(calendar.time)
 
+    // gps variable
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
+    private lateinit var geoCoder : Geocoder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
@@ -34,6 +47,42 @@ class AddActivity : AppCompatActivity() {
         db = AppDatabase.getInstance(this)
 
         tv_add_date.text = SimpleDateFormat("yyyy년 MM월 dd일 E").format(calendar.time).toString()
+
+        // 위치 체크
+        // get gps location
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        var mAddress : StringBuffer = StringBuffer()
+        geoCoder = Geocoder(this, Locale.KOREA)
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                val addresses = geoCoder.getFromLocation(location!!.latitude, location.longitude, 1)
+                for (addr: Address in addresses) {
+                    val index = addr.maxAddressLineIndex
+                    for(i : Int in arrayOf(index)){
+                        mAddress.append(addr.getAddressLine(i));
+                        mAddress.append(" ");
+                    }
+                    mAddress.append("\n");
+                }
+
+                Log.d("TEST", mAddress.toString())
+            }
+            .addOnFailureListener {exception: Exception ->
+                exception.printStackTrace()
+                Log.e("TEST", exception.message)
+            }
     }
 
     // finish write diary's content
@@ -70,14 +119,14 @@ class AddActivity : AppCompatActivity() {
             builder.run {
                 setTitle("일기 저장")
                     .setMessage(message)
-                    .setPositiveButton("확인") {dialog, which ->  }
+                    .setPositiveButton("확인") {_, _ ->  }
                     .show()
             }
         } else {
             builder.run {
                 setTitle("일기 저장")
                     .setMessage(message)
-                    .setPositiveButton("확인") {dialog, which ->
+                    .setPositiveButton("확인") { _, _ ->
 
                         val thread = Thread(addRunnable)
                         thread.start()
@@ -87,7 +136,7 @@ class AddActivity : AppCompatActivity() {
                         overridePendingTransition(0, 0)
                         finish()
                     }
-                    .setNegativeButton("취소") {dialog, which ->
+                    .setNegativeButton("취소") { _, _ ->
                     }
                     .show()
             }
--- app/src/main/java/com/strange/yourdiary/view/activity/MainActivity.kt
+++ app/src/main/java/com/strange/yourdiary/view/activity/MainActivity.kt
@@ -3,28 +3,18 @@ package com.strange.yourdiary.view.activity
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.location.Address
-import android.location.Geocoder
-import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.util.Log
 import android.widget.Toast
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.strange.yourdiary.R
 import com.strange.yourdiary.view.fragment.CalendarFragment
 import com.strange.yourdiary.view.fragment.DiaryListFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var fusedLocationClient : FusedLocationProviderClient
-    private lateinit var geoCoder : Geocoder
 
     var REQUIRED_PERMISSIONS = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
@@ -81,52 +71,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         checkRunTimePermission()
-
-        // get gps location
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-
-        var mAddress : StringBuffer = StringBuffer()
-        geoCoder = Geocoder(this, Locale.KOREA)
-
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
-                val addresses = geoCoder.getFromLocation(location!!.latitude, location.longitude, 1)
-                for (addr: Address in addresses) {
-                    val index = addr.maxAddressLineIndex
-                    for(i : Int in arrayOf(index)){
-                        mAddress.append(addr.getAddressLine(i));
-                        mAddress.append(" ");
-                    }
-                    mAddress.append("\n");
-                }
-
-                Log.d("TEST", mAddress.toString())
-            }
-            .addOnFailureListener {exception: Exception ->
-                exception.printStackTrace()
-                Log.e("TEST", exception.message)
-            }
     }
 
+    // 위치 권한 확인
     private fun checkRunTimePermission() {
-        //런타임 퍼미션 처리
         // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
         val hasFineLocationPermission = ContextCompat.checkSelfPermission(
             this@MainActivity,
@@ -139,32 +87,20 @@ class MainActivity : AppCompatActivity() {
         if (hasFineLocationPermission == PERMISSION_GRANTED &&
             hasCoarseLocationPermission == PERMISSION_GRANTED
         ) {
-
-            // 2. 이미 퍼미션을 가지고 있다면
-            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
-
-
-            // 3.  위치 값을 가져올 수 있음
-        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
-
-            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            return
+        } else {
             if (ActivityCompat.shouldShowRequestPermissionRationale(
                     this@MainActivity,
                     REQUIRED_PERMISSIONS.get(0)
                 )
             ) {
-
-                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
                 Toast.makeText(this@MainActivity, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG)
                     .show()
-                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
                 ActivityCompat.requestPermissions(
                     this@MainActivity, REQUIRED_PERMISSIONS,
                     1
                 )
             } else {
-                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
-                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
                 ActivityCompat.requestPermissions(
                     this@MainActivity, REQUIRED_PERMISSIONS,
                     1
@@ -180,37 +116,27 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 1 && permissions.size == REQUIRED_PERMISSIONS.size) {
-
-            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
-            var check_result = true
-
-
-            if (check_result) {
-                //위치 값을 가져올 수 있음
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    REQUIRED_PERMISSIONS[0]
+                )
+                || ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    REQUIRED_PERMISSIONS[1]
+                )
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
             } else {
-                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
-                if (ActivityCompat.shouldShowRequestPermissionRationale(
-                        this,
-                        REQUIRED_PERMISSIONS.get(0)
-                    )
-                    || ActivityCompat.shouldShowRequestPermissionRationale(
-                        this,
-                        REQUIRED_PERMISSIONS.get(1)
-                    )
-                ) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
-                        Toast.LENGTH_LONG
-                    ).show()
-                    finish()
-                } else {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
+                Toast.makeText(
+                    this@MainActivity,
+                    "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
