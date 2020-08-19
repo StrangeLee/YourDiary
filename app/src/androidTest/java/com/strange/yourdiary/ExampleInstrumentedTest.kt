1fcd9b5eb067fb424edc973580054fb101ab728b
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2020-08-19 19:20:19 +0900

    [FIX] error fix

--- app/src/androidTest/java/com/strange/yourdiary/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/strange/yourdiary/ExampleInstrumentedTest.kt
@@ -1,13 +1,11 @@
 package com.strange.yourdiary
 
 import androidx.test.InstrumentationRegistry.getTargetContext
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,7 +17,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.strange.yourdiary", appContext.packageName)
     }
 }
