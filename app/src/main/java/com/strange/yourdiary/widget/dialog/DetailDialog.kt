3728f652fbef5702a7fc78869a75a82239285133
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2020-08-23 17:55:22 +0900

    [ADD] DeatilDialong.kt 날짜 처리
    DeatilDialong.kt 에서의 날짜 변환 함수 추가

--- app/src/main/java/com/strange/yourdiary/widget/dialog/DetailDialog.kt
+++ app/src/main/java/com/strange/yourdiary/widget/dialog/DetailDialog.kt
@@ -10,14 +10,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.Toast
 import com.strange.yourdiary.R
 import com.strange.yourdiary.data.DiaryData
 import com.strange.yourdiary.databinding.DialogDiaryDetailBinding
 import kotlinx.android.synthetic.main.dialog_diary_detail.*
 import kotlinx.android.synthetic.main.dialog_diary_detail.view.*
+import kotlinx.android.synthetic.main.dialog_todo_edit.view.*
 import java.text.SimpleDateFormat
 
-// 2020/08/22 Todo : 날짜 처리 하기
 class DetailDialog(context: Context, private val diary: DiaryData) : Dialog(context) {
 
     private lateinit var binding: DialogDiaryDetailBinding
@@ -33,14 +34,31 @@ class DetailDialog(context: Context, private val diary: DiaryData) : Dialog(cont
             dismiss()
         }
 
-        Log.d("Data", diary.content)
-        Log.d("Data", diary.location)
-        Log.d("Data", diary.uploadDate.substring(1, 10))
-        Log.d("Data", diary.uploadDate.substring(11))
+        binding.root.tv_dialog_location.setOnClickListener {
+            Toast.makeText(context, diary.location, Toast.LENGTH_LONG).show()
+        }
+
+        convertData()
+    }
+
+    private fun convertData() {
+        val transDate = SimpleDateFormat("yyyy-MM-dd").parse(diary.uploadDate)
+        val time = diary.uploadDate.substring(11)
+
+        Log.d("Data", "location length ${diary.location.length}")
+        val location = if (diary.location.length > 15) {
+            diary.location.substring(15) + "..."
+        } else {
+            diary.location
+        }
 
         binding.root.tv_dialog_title.text = diary.title
         binding.root.tv_dialog_content.text = diary.content
-        binding.root.tv_dialog_location.text = diary.location
+        binding.root.tv_dialog_location.text = location
+        binding.root.tv_dialog_month.text = SimpleDateFormat("MMMM").format(transDate)
+        binding.root.tv_dialog_day.text = SimpleDateFormat("EEEE").format(transDate)
+        binding.root.tv_dialog_date.text = SimpleDateFormat("dd").format(transDate)
+        binding.root.tv_dialog_time.text = time
     }
 }
 
